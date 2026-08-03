d2a632f8efca1b9975374ffe2c9b5db52db9db1d
sruthilayaperla <sruthilaya.3091@gmail.com>
2026-08-03 09:25:58 +0530

    fourth commit

--- test39.py
+++ test39.py
@@ -1,10 +1,14 @@
-# *Args 
-#args: Abitary number of arguments
+#operator overloading
 import os
 import subprocess
 subprocess.run("cls",shell=True)
-def mysum(*x):
-    print("sum=",sum(x))
-mysum(1,2) 
-mysum(10,20,30)
-mysum(15,20,25,11,30)
+class point:
+    def __init__(self,x):
+        self.x=x
+    def __add__(self,other):
+        return self.x+other.x
+p1=point(10+3j)
+p2=point(7+3j)
+p3=p1+p2
+print("sum=",p3)
+
