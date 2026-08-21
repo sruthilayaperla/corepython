18861a7ac1aa364af9fd89962b36d2defa789be6
sruthilayaperla <sruthilaya.3091@gmail.com>
2026-08-21 09:59:52 +0530

    Update test55.py

--- numpy/test55.py
+++ numpy/test55.py
@@ -1,7 +1,7 @@
 # data visualizing using numbers:
 #line plot: A graph that displays data points connected by lines, commonly used to show trends.
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib import pyplot as plt
 x=np.arange(1,11)
 y=2*x+5
 plt.plot(x,y)
